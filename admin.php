05e8d86fa2258a1a7f739d64aecf2c1cc9e089f9
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2015-05-14 16:16:51 +0200

    Add admin.php

---
+++ admin.php
@@ -0,0 +1,60 @@
+<?php
+  session_start();
+  include('db.php');
+?>
+
+<?php
+  echo print_r($_POST);
+
+  if ($_POST['username']) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT id, user, password FROM users WHERE username=$username AND password=$password");
+
+    $result = mysqli($link, $sql);
+
+    if ($row = mysql_fetch_array($result) {
+      $_SESSION['id'] = $row['id'];
+    } else {
+      $error = "Invalid email or password";
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+        <h1>Logga in</h1>
+        <?php
+          if(error) {
+            echo "<p>$error</p>";
+          }
+        ?>
+          <form method="post" action="" class="form-group">
+            <label for="username">Användarnamn</label>
+            <input type="text" name="username" class="form-control">
+            <label for="password">Lösenord</label>
+            <input type="password" name="password" class="form-control">
+            <button type="submit" class="btn btn-default">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
--- webbartikel.php
+++ webbartikel.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
-   "http://www.w3.org/TR/html4/strict.dtd">
+<?php
+  include('db.php');
+?>
+
+<!DOCTYPE html>
 <html>
-	<head>
-	    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-		<title>Äspåröds Äventyrsblad!</title>
-		<link type="text/css" rel="stylesheet" href="blad.css" />
-	</head>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Äspåröds Äventyrsblad!</title>
+    <link type="text/css" rel="stylesheet" href="blad.css" />
+  </head>
 
 	<body>
 
@@ -15,75 +18,39 @@
 	</div>
 
 	<div id="lnav">
-		<?php
-$link = mysqli_connect('195.178.235.60', 'm11k5638', 'jonasremgard');
-if (!$link)
-{
-	echo('Unable to connect to the database server.');
-	exit();
-}
+	<?php
 
-if (!mysqli_set_charset($link, 'utf8'))
-{
-	echo('Unable to set database connection encoding.');
-	exit();
-}
+    $sql = "select Kategorinamn, SubKategorinamn, SubKategoriID from Kategori inner join Sub_Kategori on Sub_Kategori.KategoriID=Kategori.KategoriID order by Kategori.KategoriID";
+    $result = mysqli_query($link, $sql);
+    if (!$result)
+    {
+    	echo('Error processing query: ' . mysqli_error($link));
+    	exit();
+    }
 
-if (!mysqli_select_db($link, 'm11k5638'))
-{
-	echo('Unable to locate the company database.');
-	exit();
-}
+    $owner = "";
 
-$sql = "select Kategorinamn, SubKategorinamn, SubKategoriID from Kategori inner join Sub_Kategori on Sub_Kategori.KategoriID=Kategori.KategoriID order by Kategori.KategoriID";
-$result = mysqli_query($link, $sql);
-if (!$result)
-{
-	echo('Error processing query: ' . mysqli_error($link));
-	exit();
-}
-
-$owner = "";
-
-echo("<ul>\n");
-while ($row = mysqli_fetch_array($result))
-{
-	if (($row[0]!=$owner) && ($owner!="")){
-			echo("\t</ul>\n</li>\n");
-		}
-	if ($row[0]!=$owner){
-		echo('<li><a href="">'.$row[0]."</a>\n\t<ul>\n");
-		$owner=$row[0];
-	}
-	echo('<li><a href="webbtidning.php?cat='.$row[2].'">'.$row[1].'</a></li>');
+    echo("<ul>\n");
+    while ($row = mysqli_fetch_array($result))
+    {
+    	if (($row[0]!=$owner) && ($owner!="")){
+    			echo("\t</ul>\n</li>\n");
+    		}
+    	if ($row[0]!=$owner){
+    		echo('<li><a href="">'.$row[0]."</a>\n\t<ul>\n");
+    		$owner=$row[0];
+    	}
+    	echo('<li><a href="webbtidning.php?cat='.$row[2].'">'.$row[1].'</a></li>');
 
 
-}
-echo("\t</ul>\n</li>\n");
-echo("</ul>\n");
-?>
-	</div>
+    }
+    echo("\t</ul>\n</li>\n");
+    echo("</ul>\n");
+  ?>
+  </div>
 
 	<div id="artikel">
 		<?php
-$link = mysqli_connect('195.178.235.60', 'm11k5638', 'jonasremgard');
-if (!$link)
-{
-	echo('Unable to connect to the database server.');
-	exit();
-}
-
-if (!mysqli_set_charset($link, 'utf8'))
-{
-	echo('Unable to set database connection encoding.');
-	exit();
-}
-
-if (!mysqli_select_db($link, 'm11k5638'))
-{
-	echo('Unable to locate the company database.');
-	exit();
-}
 
 $ArtID = $_GET['ArtID'];
 
